21f57e1ad0423a2535e45dbcb5a679174fade4ea
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 00:57:22 -0800

    Update class-rae-register-auth-api.php

--- apis/class-rae-register-auth-api.php
+++ apis/class-rae-register-auth-api.php
@@ -1,32 +1,12 @@
-<?php
-/**
- * User Authentication REST API
- *
- * @package REST API ENDPOINTS
- */
-
+<?php 
 class Rae_Register_Auth_API {
-
-	/**
-	 * Rae_Register_Auth_API constructor.
-	 */
-	public function __construct() {
+	public function __construct()
+	{
 		add_action( 'rest_api_init', array( $this, 'rae_rest_user_endpoints' ) );
 	}
 
-	/**
-	 * Register user endpoints.
-	 */
-	function rae_rest_user_endpoints() {
-		/**
-		 * Handle User Login request.
-		 *
-		 * This endpoint takes 'username' and 'password' in the body of the request.
-		 * Returns the user object on success
-		 * Also handles error by returning the relevant error if the fields are empty or credentials don't match.
-		 *
-		 * Example: http://example.com/wp-json/wp/v2/rae/user/login
-		 */
+	function rae_rest_user_endpoints() 
+	{
 		register_rest_route(
 			'wp/v2/rae',
 			'/user/login',
@@ -36,22 +16,16 @@ class Rae_Register_Auth_API {
 		));
 	}
 
-	/**
-	 * User Login call back.
-	 *
-	 * @param WP_REST_Request $request
-	 */
 	function rae_rest_user_login_endpoint_handler( WP_REST_Request $request ) {
 		$response = array();
 		$parameters = $request->get_params();
-
+		
 		$username = sanitize_text_field( $parameters['username'] );
 		$password = sanitize_text_field( $parameters['password'] );
-
-		// Error Handling.
 		$error = new WP_Error();
 
-		if ( empty( $username ) ) {
+		if ( empty( $username ) ) 
+		{
 			$error->add(
 				400,
 				__( "Username field is required", 'rest-api-endpoints' ),
@@ -61,7 +35,8 @@ class Rae_Register_Auth_API {
 			return $error;
 		}
 
-		if ( empty( $password ) ) {
+		if ( empty( $password ) ) 
+		{
 			$error->add(
 				400,
 				__( "Password field is required", 'rest-api-endpoints' ),
@@ -73,16 +48,16 @@ class Rae_Register_Auth_API {
 
 		$user = wp_authenticate( $username, $password  );
 
-		// If user found
-		if ( ! is_wp_error( $user ) ) {
+		if ( ! is_wp_error( $user ) ) 
+		{
 			$response['status'] = 200;
 			$response['user'] = $user;
-		} else {
-			// If user not found
+		} 
+		else 
+		{
 			$error->add( 406, __( 'User not found. Check credentials', 'rest-api-endpoints' ) );
 			return $error;
 		}
-
 		return new WP_REST_Response( $response );
 	}
 }
