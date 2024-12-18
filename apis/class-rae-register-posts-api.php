7bd2df0c25d4d9de54ba145ab95b630703425537
nicholas harper <nicholasharper000@gmail.com>
2024-12-17 22:12:08 -0800

    Update class-rae-register-posts-api.php

--- apis/class-rae-register-posts-api.php
+++ apis/class-rae-register-posts-api.php
@@ -1,11 +1,13 @@
 <?php
-class Rae_Register_Posts_Api {
+class Rae_Register_Posts_Api 
+{
 	function __construct() 
 	{
 		add_action( 'rest_api_init', array( $this, 'rae_rest_posts_endpoints' ) );
 	}
 
-	function rae_rest_posts_endpoints() {
+	function rae_rest_posts_endpoints() 
+	{
 		register_rest_route(
 			'wp/v2/rae',
 			'/post/create',
@@ -20,27 +22,23 @@ class Rae_Register_Posts_Api {
 		$response = array();
 		$parameters = $request->get_params();
 		$user_id = sanitize_text_field( $parameters['user_id'] );
-		$title = sanitize_text_field( $parameters['title'] );
 		$content = sanitize_text_field( $parameters['content'] );
+		$title = sanitize_text_field( $parameters['title'] );
 
-		// Error Handling.
 		$error = new WP_Error();
 
 		if ( empty( $user_id ) ) 
 		{
-			$error->add(
-				400,
+			$error->add(400,
 				__( "User ID field is required", 'rest-api-endpoints' ),
-				array( 'status' => 400 )
-			);
+				array( 'status' => 400 ));
 
 			return $error;
 		}
 
-		if ( empty( $title ) ) 
+		if (empty($title)) 
 		{
-			$error->add(
-				400,
+			$error->add(400,
 				__( "Title field is required", 'rest-api-endpoints' ),
 				array( 'status' => 400 )
 			);
@@ -48,10 +46,9 @@ class Rae_Register_Posts_Api {
 			return $error;
 		}
 
-		if ( empty( $content ) ) 
+		if (empty($content)) 
 		{
-			$error->add(
-				400,
+			$error->add(400,
 				__( "Body field is required", 'rest-api-endpoints' ),
 				array( 'status' => 400 )
 			);
@@ -62,8 +59,7 @@ class Rae_Register_Posts_Api {
 		$user_can_publish_post = user_can( $user_id,'publish_posts' );
 		if ( ! $user_can_publish_post ) 
 		{
-			$error->add(
-				400,
+			$error->add(400,
 				__( "You don't have previlige to publish a post", 'rest-api-endpoints' ),
 				array( 'status' => 400 )
 			);
@@ -81,7 +77,7 @@ class Rae_Register_Posts_Api {
 		
 		$post_id = wp_insert_post( $my_post );
 		
-		if ( ! is_wp_error( $post_id ) ) 
+		if ( !is_wp_error($post_id) ) 
 		{
 			$response['status'] = 200;
 			$response['post_id'] = $post_id;
